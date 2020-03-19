83e7a412580597fb13e714e3bde76ea864b32675
Aslmmon <Aslmmon993@gmail.com>
2020-03-19 10:22:19 +0200

    * added Bottom sheet to Create Task

--- app/src/main/java/com/example/taskati/common/Navigation.kt
+++ app/src/main/java/com/example/taskati/common/Navigation.kt
@@ -7,8 +7,10 @@ import com.example.taskati.features.login.home.HomeActivity
 
 object Navigation {
 
-    fun goToHomeActivity(ctx: Context) {
-        ctx.startActivity(Intent(ctx, HomeActivity::class.java))
+    fun goToHomeActivity(ctx: Context,uid:String) {
+        val intent = Intent(ctx, HomeActivity::class.java)
+        intent.putExtra("uid",uid)
+        ctx.startActivity(intent)
         (ctx as Activity).finish()
     }
 }
--- app/src/main/java/com/example/taskati/features/login/home/HomeActivity.kt
+++ app/src/main/java/com/example/taskati/features/login/home/HomeActivity.kt
@@ -3,10 +3,25 @@ package com.example.taskati.features.login.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.taskati.R
+import com.example.taskati.common.bases.setSafeOnClickListener
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.button.MaterialButton
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity(R.layout.activity_home) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        fab_btn_add.setOnClickListener {
+            val dialog = BottomSheetDialog(this)
+            val view = layoutInflater.inflate(R.layout.taks_bottom_sheet, null)
+            val create_btn = view.findViewById<MaterialButton>(R.id.btn_create_task)
+            create_btn.setSafeOnClickListener {
+                dialog.dismiss()
+            }
+            dialog.setContentView(view)
+            dialog.show()
+        }
     }
 }
